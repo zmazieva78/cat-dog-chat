7540e588ad9cec38f43c42b27b10963d9821a922
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-23 14:08:18 -0700

    remove <li> tags

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,32 @@ $(document).ready(function() {
   $("#hello").click(function() {
     $("ul.cat").prepend("<li>Meow</li>");
     $("ul.dog").prepend("<li>Woof</li>");
+    $("ul.cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul.dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
   $("#food").click(function() {
     $("ul.cat").prepend("<li>Fish</li>");
     $("ul.dog").prepend("<li>Lamb</li>");
+    $("ul.cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul.dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
   $("#temper").click(function() {
     $("ul.cat").prepend("<li>If I am in my good mood.</li>");
     $("ul.dog").prepend("<li>I am always friendly, no matter what</li>");
+    $("ul.cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul.dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
 
